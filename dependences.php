db84f0092dddad2d6fec3a033ac6039f70f249e1
Paulo Eremita <eremitaio79@gmail.com>
2025-01-25 15:20:22 -0300

    Primeiros arquivos criados
    Primeiros arquivos criados.

---
+++ dependences.php
@@ -0,0 +1,2 @@
+<link href="./css/bootstrap5.3.0/bootstrap.min.css" rel="stylesheet">
+<script src="./js/bootstrap5.3.0/bootstrap.bundle.min.js"></script>
---
+++ upload-exec.php
@@ -0,0 +1,34 @@
+<?php
+include_once "./config.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= SYSTEM_TITLE; ?></title>
+
+    <?php include_once "./dependences.php"; ?>
+</head>
+
+<body>
+    <header>
+        <?php include_once "./layout/navbar.php"; ?>
+    </header>
+
+    <main class="container my-5">
+        <div class="row">
+            <div class="col-12">
+                xxx
+            </div>
+        </div>
+    </main>
+
+
+</body>
+
+</html>
